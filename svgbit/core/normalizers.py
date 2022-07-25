b3cc9a36c28392c9cff1807715dc8d7de1f98dca
fuyu-sama <yingzhouhong@outlook.com>
2022-07-25 09:21:30 +0800

    Add normalizers

---
+++ svgbit/core/normalizers.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import numpy as np
+from numpy import log as nlog
+
+from .STDataset import STDataset
+
+
+def cpm_normalizer(dataset: STDataset) -> STDataset:
+    scale_df = (dataset.count_df.T * 10000 / dataset.count_df.T.sum())
+    return STDataset(
+        scale_df.T,
+        dataset.coordinate_df,
+    )
+
+
+def logcpm_normalizer(dataset: STDataset) -> STDataset:
+    scale_df = nlog(dataset.count_df.T * 10000 / dataset.count_df.T.sum() + 1)
+    return STDataset(
+        scale_df.T,
+        dataset.coordinate_df,
+    )
---
+++ svgbit/normalizers.py
@@ -0,0 +1,1 @@
+from .core.normalizers import logcpm_normalizer
